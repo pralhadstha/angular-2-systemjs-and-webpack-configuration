66232d6ffc504ec7cb4db76c45921499149ee1ba
Pralhad Shrestha <pralhad.shrestha05@gmail.com>
2016-11-09 12:50:14 +0545

    Template for data passing

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app',
 	template: `
-	Hello World From Component Updated twice!!!
+	<main>
+		<div>
+			{{ loginmessage }}
+		</div>
+		<div class="arrow">
+			<div *ngIf = "users">
+				<div *ngFor = "let user of users">
+					{{ user.id }} {{ user.name }}
+				</div>
+			</div>
+		</div>
+	</main>
 	`
 })
 
-export class AppComponent {}
+export class AppComponent {
+	loginmessage = "This is a login message!";
+	users = [
+		{ id: 1, name: 'Apple'},
+		{ id: 2, name: 'Ball'},
+		{ id: 3, name: 'Cat'},
+	]
+}
