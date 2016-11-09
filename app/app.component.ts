13b2e4f66e18e3652c8b6aeaf7dc090a14aa2eea
Pralhad Shrestha <pralhad.shrestha05@gmail.com>
2016-11-09 13:25:54 +0545

    Playing with all the structural directives, event binding and property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,15 @@ import { Component } from '@angular/core';
 		</div>
 		<div class="arrow">
 			<div *ngIf = "users">
-				<div *ngFor = "let user of users">
+				<div *ngFor = "let user of users" (click) = "selectUser(user)">
+					<!--
+						structural directives -> *
+						event binding -> ()
+						property binding -> []
+
+						Eg.:
+						[class.active] = "user ===activeUser"
+					-->
 					{{ user.id }} {{ user.name }}
 				</div>
 			</div>
@@ -24,5 +32,13 @@ export class AppComponent {
 		{ id: 1, name: 'Apple'},
 		{ id: 2, name: 'Ball'},
 		{ id: 3, name: 'Cat'},
-	]
+	];
+
+	activeUser; // variable declaration which automatically adds to the class
+
+	selectUser(user) {
+		this.activeUser = user;
+		console.log(this.activeUser);
+	}
+
 }
